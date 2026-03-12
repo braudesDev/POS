4ff76a688e1cbfd535362d36039440220e446253
Braulio Rodríguez García <braudesdev@gmail.com>
2026-03-12 03:17:14 -0600

    chore: Dejar de trackear environment files y añadir ejemplos

---
+++ src/environments/environments.example.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'YOUR_API_KEY_HERE',
+    authDomain: 'your-auth-domain.firebaseapp.com',
+    projectId: 'your-project-id',
+    storageBucket: 'your-storage-bucket.firebasestorage.app',
+  }
+};
---
+++ src/environments/environments.prod.example.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'YOUR_API_KEY_HERE',
+    authDomain: 'your-auth-domain.firebaseapp.com',
+    projectId: 'your-project-id',
+    storageBucket: 'your-storage-bucket.firebasestorage.app',
+  }
+};
